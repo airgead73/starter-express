bd3b31355e3ae690b41a73b4cbe46eade4a730b2
Dan M <moneypenny.dan@gmail.com>
2022-11-11 01:26:17 -0800

    Update app.js

--- app/app.js
+++ app/app.js
@@ -49,8 +49,6 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
 app.use(session(sessionConfig));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 
 /**
  * locals
