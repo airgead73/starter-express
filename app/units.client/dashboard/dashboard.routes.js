2e2c7a7faf1d4b55521b205f22b3b7f188fed359
Dan M <moneypenny.dan@gmail.com>
2022-11-11 01:20:45 -0800

    Delete app/units.client directory

--- app/units.client/dashboard/dashboard.routes.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const dashboardRouter = Router();
-
-dashboardRouter.use('/', (req,res,next) => {
-  res.send('CLIENT: dashboard');
-});
-
-module.exports = dashboardRouter;
