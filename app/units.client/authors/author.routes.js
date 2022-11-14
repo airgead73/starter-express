bdcc1e3c3e48c160b689dc8ef0e382ac173ba58e
Dan Moneypenny <catullus1973@gmail.com>
2022-11-14 07:09:33 -0800

    update

--- app/units.client/authors/author.routes.js
+++ app/units.client/authors/author.routes.js
@@ -4,13 +4,19 @@ const authorRouter = Router();
 // controllers
 const { create, read, detail, update, remove, drop } = require('./author.controller');
 
+// model
+const { Author } = require('../../units.api/authors');
+
+// middleware
+const { checkID } = require('../../middleware');
+
 authorRouter.get('/', read);
 
 authorRouter.get('/add', create);
 
 authorRouter.get('/drop', drop);
 
-authorRouter.get('/:id', detail);
+authorRouter.get('/:id', checkID(Author, 'author'), detail);
 
 authorRouter.get('/:id/update', update);
 
