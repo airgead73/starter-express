57019cc308ea4c184005ff33dfae10970da4843c
Dan Moneypenny <catullus1973@gmail.com>
2022-11-18 04:32:53 -0800

    update

--- app/middleware/handleError.js
+++ app/middleware/handleError.js
@@ -5,14 +5,25 @@ const handleError = (err,req,res,next) => {
   } else {
     status = 500;
   }
-  res.status(status).json({
-    success: false,
-    status: status,
-    name: err.name,
-    message: err.message,
-    path: req.path,
-    stack: err.stack
-  });
+  // res.status(status).json({
+  //   success: false,
+  //   status: status,
+  //   name: err.name,
+  //   message: err.message,
+  //   path: req.path,
+  //   stack: err.stack
+  // });
+  res.status(status)
+    .render('template', {
+      success: false,
+      pagePath: './pages/error',
+      title: `error: ${status}`,      
+      status: status,
+      name: err.name,
+      message: err.message,
+      path: req.path,
+      stack: err.stack     
+    })
 }
 
 module.exports = handleError;
